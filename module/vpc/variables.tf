c2317794b15fb6214458d9c82b7d93d9f95e8ebd
Tejas1045 <82049342+Tejas1045@users.noreply.github.com>
2022-09-01 18:19:53 +0530

    Update variables.tf

--- module/vpc/variables.tf
+++ module/vpc/variables.tf
@@ -2,3 +2,11 @@ variable "region" {
   type    = string
   default = "us-east-1"
 }
+variable "access_key" {
+  type    = string
+  default = "AKIAZ32QFUO4ONWFVUVY"
+}
+variable "secret_key" {
+  type    = string
+  default = "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"
+}
