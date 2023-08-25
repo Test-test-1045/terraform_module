8be9f3353f4514250fbc14116bfe225a8c61c436
tejas1045 <tejasnalawade45@gmail.com>
2023-08-25 10:34:05 +0530

    modifiying script

--- delete.sh
+++ delete.sh
@@ -5,17 +5,25 @@
     do
      echo $branch
      lastDate=$(git show -s --format=%ci origin/$branch)
-     echo last date=$lastDate
+     #echo last date=$lastDate
      convertDate=$(echo $lastDate | cut -d' ' -f 1)
-     echo  convertDate=$convertDate
+     #echo  convertDate=$convertDate
      Todate=$(date -d "$convertDate" +'%s') #%s converts the date to seconds till date
-     echo  Todate=$Todate
+     #echo  Todate=$Todate
      current=$(date +'%s')
-     echo current=$current
+     #echo current=$current
      day=$(( ( $current - $Todate )/60/60/24 ))
      echo "last commit on $branch branch was $day days ago"
      if [ "$day" -gt 20 ]; then
-        echo "delete the old branch $branch"
+        echo "achieving the old branches $branch" 
+        git tag archive/$branch $branch
+        git push --tags
+
+     fi
+     if [ "$day" -gt 25 ]; then
+        echo "deleting the old branch $branch"
+        git branch -d $branch
+        git push origin :$branch
      fi
      echo -----------------------------------------------------------------------
     done
